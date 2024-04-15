afefe99c0a4a93cd06a01142fe701329025c7e60
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-04-15 10:12:48 +0200

    feat: initialize run file

---
+++ src/run.py
@@ -0,0 +1,48 @@
+import argparse
+
+DEFAULT_TIME_LIMIT_S = 600
+
+parser = argparse.ArgumentParser(
+    description="A tool for testing and comparing qt, q-synth, olsq2, tb-olsq2 and sabre.",
+    prog="./run",
+)
+
+parser.add_argument(
+    "-t",
+    "--time_limit",
+    type=int,
+    help=f"the time limit in seconds, default is {DEFAULT_TIME_LIMIT_S}s",
+    default=DEFAULT_TIME_LIMIT_S,
+)
+
+parser.add_argument(
+    "-cx",
+    "--cx_optimal",
+    help=f"whether to optimize for cx-depth",
+    action="store_true",
+)
+
+parser.add_argument(
+    "-swap",
+    "--swap_optimal",
+    help=f"whether to optimize for swap count after finding a depth-optimal circuit",
+    action="store_true",
+)
+
+parser.add_argument(
+    "tool",
+    type=str,
+    help="the tool to use, one of 'qt', 'q-synth', 'olsq2', 'tb-olsq2', 'sabre'",
+)
+
+parser.add_argument(
+    "input",
+    type=str,
+    help="the path to the input file",
+)
+
+parser.add_argument(
+    "platform",
+    type=str,
+    help="the platform to run on",
+)

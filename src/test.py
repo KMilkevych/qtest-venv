a46a776cb59786f0d28f9a497f6d74241d9a647c
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-04-16 22:23:55 +0200

    refactor: moved circuits utils in own file

--- src/test.py
+++ src/test.py
@@ -2,9 +2,10 @@ import os
 import subprocess
 import argparse
 import datetime
-import time
 
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit
+from circuits import with_swaps_as_cnots, remove_all_non_cx_gates, count_swaps
+
 
 DEFAULT_TIME_LIMIT_S = 600
 TOOLS = ["qt", "q-synth", "olsq2", "tb-olsq2", "sabre"]
@@ -19,48 +20,6 @@ def run(command: str, path: str):
     return subprocess.check_output(
         f"cd {path}; source .venv/bin/activate; {command}", shell=True
     ).decode("utf-8")
-
-
-def remove_all_non_cx_gates(circuit: QuantumCircuit) -> QuantumCircuit:
-    """
-    Remove all non-CX gates from the circuit.
-    """
-    num_qubits = circuit.num_qubits
-    qubit_name = circuit.qregs[0].name
-    new_circuit = QuantumCircuit(QuantumRegister(num_qubits, qubit_name))
-    for instr in circuit.data:
-        if instr[0].name == "cx":
-            new_circuit.append(instr[0], instr[1])
-
-    return new_circuit
-
-
-def count_swaps(circuit: QuantumCircuit):
-    """
-    Counts SWAP gates in a circuit.
-    """
-    swaps = 0
-    for instr in circuit.data:
-        if instr[0].name.startswith("swap"):
-            swaps += 1
-
-    return swaps
-
-
-def with_swaps_as_cnots(circuit: QuantumCircuit, register_name: str):
-    """
-    Replaces all SWAP gates with CNOT gates.
-    """
-    new_circuit = QuantumCircuit(QuantumRegister(circuit.num_qubits, register_name))
-    for instr in circuit.data:
-        if instr[0].name.startswith("swap"):
-            new_circuit.cx(instr[1][0]._index, instr[1][1]._index)
-            new_circuit.cx(instr[1][1]._index, instr[1][0]._index)
-            new_circuit.cx(instr[1][0]._index, instr[1][1]._index)
-        else:
-            new_circuit.append(instr[0], instr[1])
-
-    return new_circuit
 
 
 parser = argparse.ArgumentParser(
